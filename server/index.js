882e279f5429e64d273a9d032fea6cdc9fa95b39
Ahmad Elshowair <it.is.work@outlook.com>
2022-06-26 19:54:13 +0200

    add input component

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import pool  from './db.js';
 
 const app = express();
-const port = 3000
+const port = 8000
 
 // ============== middleware ============== //
 app.use(cors());
