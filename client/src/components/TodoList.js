1caea16fc3bd633e4165a58fb68ba3d23ee7990e
Ahmad Elshowair <it.is.work@outlook.com>
2022-06-27 05:14:50 +0200

    add todo list component

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,16 +1,61 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { EditTodo } from "./EditTodo";
+
+
 export const TodoList = () =>{
+
+  const [todos, setTodos] = React.useState([]);
+
+  // get all todos
+  const getTodos = async() =>{
+    try {
+      const response = await fetch('http://localhost:8000/todos')
+      const data = await response.json();
+      setTodos(data);
+    } catch (error) {
+      throw new Error(`the error is: ${error.message}`);
+    }
+  }
+
+  React.useEffect(()=>{
+    getTodos();
+  },[todos]);
+
+  // delete a todo
+ const handleDeleteAction = async (id)=>{
+  try{
+      await fetch(`http://localhost:8000/todos/delete/${id}`,{
+        method: "DELETE"
+      });
+  }catch(error){
+    throw new Error(`the error is: ${error.message}`);
+  }
+ }
+
  return (
   <section className="todo--list mt-5">
-    <table className="table table-secondary">
+    <table className="table table-striped ">
       <thead>
         <tr>
           <th>Description</th>
-          <th>Edit</th>
-          <th>Delete</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
-        {}
+        {todos.map((todo)=>{
+          return(
+            <tr key={todo.todo_id}>
+              <td>
+                {todo.description}
+              </td>
+              <td className="d-flex justify-content-between">
+                <EditTodo todo={todo}/>
+                <button className="btn btn-danger" onClick={() =>handleDeleteAction(todo.todo_id)}>Delete</button>
+              </td>
+            </tr>
+          )
+        })}
       </tbody>
     </table>
   </section>
